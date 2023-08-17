0cbee18694c24b27161d8fa94e8f6c3b71ed5447
Victor Romero-Beltran <139508338+vromero-beltran@users.noreply.github.com>
2023-08-17 14:50:14 -0700

    options function

--- assets/script.js
+++ assets/script.js
@@ -1,8 +1,13 @@
 var questions = [
     {
         question: "What is the color of the sun when it is at its coolest point?",
-        option:["White","Blue","Red","Yellow"],
-        answerIndex: 2
+        option:[
+            "White",
+            "Blue",
+            "Red",
+            "Yellow"
+        ],
+        answerIndex: 'Red'
     },
     {
         question: "What is the color of the sun when it is at a medium point of heat?",
@@ -12,7 +17,7 @@ var questions = [
             "Yellow",
             "White"
         ],
-        answerIndex: 3
+        answerIndex: 'White'
     },
     {
         question: "What is the color of the sun at its hottest point?",
@@ -22,7 +27,7 @@ var questions = [
             "White",
             "Red"
         ],
-        answerIndex: 1
+        answerIndex: 'Blue'
     },
     {
         question: "When looking at the Sun from Earth, What color is it? (Please don't stare at the Sun.)",
@@ -32,7 +37,7 @@ var questions = [
             "Red",
             "Blue"
         ],
-        answerIndex: 0
+        answerIndex: 'White'
     },
     {
         question: "When a painter wants to use a cooler colored paint, what color is that?",
@@ -42,7 +47,7 @@ var questions = [
             "Blue",
             "Yellow"
         ],
-        answerIndex: 2
+        answerIndex: 'Blue'
     },
     {
         question: "What color scatters the least from the sunlight when it hits the atmosphere?",
@@ -52,7 +57,7 @@ var questions = [
             "Violet",
             "Green"
         ],
-        answerIndex: 1
+        answerIndex: 'Red'
     },
     {
         question: "When sunlight hits the atmosphere the shortest wavelengths are scattered the most, what color is scattered the most but you don't see it in the sky?",
@@ -62,7 +67,7 @@ var questions = [
             "indigo",
             "Violet"
         ],
-        answerIndex: 3
+        answerIndex: 'Violet'
     },
     {
         question: "What color light travels the least in the dark?",
@@ -72,7 +77,7 @@ var questions = [
             "Yellow",
             "Orange"
         ],
-        answerIndex: 1
+        answerIndex: 'Red'
     },
     {
         question: "What color light helps you stay up in the dark/night?",
@@ -82,12 +87,17 @@ var questions = [
             "Blue",
             "Yellow"
         ],
-        answerIndex: 2
+        answerIndex: 'Blue'
     },
     {
         question: "What wavelength of light does grass not absorb as energy?",
-        option:["Blue Wavelength","Violet Wavelength","Indigo Wavelength","Green Wavelength"],
-        answerIndex: 3
+        option:[
+            "Blue Wavelength",
+            "Violet Wavelength",
+            "Indigo Wavelength",
+            "Green Wavelength"
+        ],
+        answerIndex: 'Green Wavelength'
     }
 ];
 // These are my Global Variables
@@ -109,9 +119,9 @@ startButton.addEventListener('click', startQuiz);
 function startQuiz() {
     timeLeft = 70; // This sets time and starts timer
     startTimer ();
-    document.getElementById('start-btn').style.display = 'none'; 
+    document.getElementById('start-btn').style.display = 'none'; // makes the start button disapear.
     showQuestion(); // Display first question and options
-    displayOptions();
+//    displayOptions();
 }
 
 function startTimer () {
@@ -122,26 +132,35 @@ function startTimer () {
             endQuiz();
         }
     }, 1000);
-    console.log('is the startTimer function working?')
+    
 }
 
 function showQuestion() { // code to display the current question and option DOM element.
-    for (let i = 0; i < questions.length; i++) { // loops through each question in the question array
-        const element = questions[i]; // stores curent question object in element
-        console.log("element", element); // logs the current question object
-        questionElement.innerText = questions[i].question; // sets question div text to current queston text
+    for (let i = 0; i < questions.length; i++) { // loops through each question in the question array.
+        // console.log(questions);
+        const element = questions[i]; // stores curent question object in element.
+        console.log("element", element); // logs the current question object.
+        questionElement.innerText = questions[i].question; // sets question div text to current queston text.
+        for (let x = 0; x < questions[i].option.length; x++) {
+            let newOption = document.createElement('button'); // creates a new button tag with an id of optionX where X is the index number
+            newOption.setAttribute('class', 'answers');
+            newOption.setAttribute('value', questions[i].option[x]);
+            newOption.textContent = questions[i].option[x];
+            optionElement.append = newOption;
+        };
     };
-    console.log('is the showQuestion function working?')
+    
+    
 }
 
-function displayOptions() {
-    for (let i = 0; i < displayOptions.length; i++) {
-        const element = options[i];
-        console.log('element', element);
-        optionElement.innerText = options[i].option;
-    };
-    console.log('is the displayOptions function working?')
-}
+//function displayOptions() {
+//    for (let i = 0; i < displayOptions.length; i++) {
+//        const element = options[i];
+//        console.log('element', element);
+//        optionElement.innerText = options[i].option;
+//    };
+//    console.log('is the displayOptions function working?')
+//}
 
 function checkAnswer(answer) {
     if ((questions[currentQuestionIndex].option)[answer] === (questions[currentQuestionIndex].correct)) {
@@ -165,8 +184,8 @@ function endQuiz () {
 function saveLastHighScore() {
     // Save related form data as an object
     var highScore = {
-      score: score.value,
-      initials: initials.value
+        score: score.value,
+        initials: initials.value
     };
     // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
     localStorage.setItem('highscore', JSON.stringify(highScore));
@@ -177,24 +196,20 @@ function renderLastHighScore() {
     var lastHighScore = JSON.parse(localStorage.getItem('highScore'));
     // Check if data is returned, if not exit out of the function
     if (lastHighScore !== null) {
-      document.getElementById('saved-score').innerHTML = lastHighScore.score;
-      document.getElementById('saved-initials').innerHTML = lastHighScore.initials;
+        document.getElementById('saved-score').innerHTML = lastHighScore.score;
+        document.getElementById('saved-initials').innerHTML = lastHighScore.initials;
     }
 }
 
 function displayLastHighScore () {
     var highScores = JSON.parse(localStorage.getItem('highScore')) || [];
-
     highScores.sort(function(a,b) {
         return b.score - a.score;
     });
-
     var topScore = highScores[0];
-
     if (topScore) {
         var highScoreEl =document.querySelector('final-score');
         highScoreEl.textContent = topScore.initials + ' - ' + topScore.score;
-
     } else {
         var highScoreEl = document.querySelector('#final-score');
         highScoreEl.textContent = 'No Scores Yet';
